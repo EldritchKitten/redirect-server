daf57bc25d5613e8cabd417c4e1a3fcb9a55c74c
EldritchKitten <violetjadebrown@proton.me>
2025-08-15 14:36:34 +0100

    Add error responses and refactor.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::{
-    env,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
+    collections::HashMap, env, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}
 };
 
 struct Args {
@@ -26,10 +24,24 @@ impl Args {
     }
 }
 
+struct Redirects(HashMap<String, String>);
+impl Redirects {
+    pub fn collect() -> Redirects {
+        let mut map = HashMap::new();
+        map.insert("collectio.nz".to_string(), "http://localhost:8000".to_string());
+        Redirects(map)
+    }
+
+    pub fn get(&self, host: &str) -> Option<&str> {
+        self.0.get(host).map(String::as_str)
+    }
+}
+
 fn main() {
     println!("Initializing...");
 
     let args = Args::collect();
+    let redirects = Redirects::collect();
 
     let listener = TcpListener::bind(&args.binding)
         .expect("Failed to bind TCP listener");
@@ -40,54 +52,77 @@ fn main() {
         request_counter += 1;
         println!("=== Request {request_counter} ===");
         match incoming_request {
-            Ok(stream) => handle_connection(stream),
+            Ok(stream) => handle_connection(stream, &redirects),
             Err(err) => println!("Error: {}", err),
         }
         println!("================");
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    
-    for line in http_request {
-        println!("{}", line);
-    }
-
-    let response = response_redirect();
-
+fn handle_connection(mut stream: TcpStream, redirects: &Redirects) {
+    let request = parse_request(&stream);
+    log_request(&request);
+    let response = determine_response(&request, redirects);
     println!("=== Response ===");
     println!("{}", &response);
-
     stream.write_all(response.as_bytes()).unwrap();
 }
 
-fn response_redirect() -> String {
-    let location = "http://localhost:8000";
-
-    let location_header = format!("Location: {location}");
-    let headers = format!("{location_header}");
-
-    let status_line = "HTTP/1.1 308 Permanent Redirect";
-
-    return format!("{status_line}\r\n{headers}");
+fn parse_request(stream: &TcpStream) -> Vec<String> {
+    BufReader::new(stream)
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect()
 }
 
-fn response_possessed() -> String {
-    let content = ":3";
-    let length = content.len();
+fn log_request(request: &Vec<String>) {
+    for line in request {
+        println!("{}", line);
+    }
+}
 
-    let length_header = format!("Content-Length: {length}");
-    let type_header = format!("Content-Type: text/plain");
-    let headers = format!("{length_header}\r\n{type_header}");
+fn determine_response(request: &Vec<String>, redirects: &Redirects) -> String {
+    match find_host(request) {
+        Some(host) => {
+            match redirects.get(&host) {
+                Some(redirect_location) => response_redirect(redirect_location),
+                None => response_host_not_configured(&host),
+            }
+        },
+        None => response_host_not_specified(),
+    }
+}
 
-    let status_line = "HTTP/1.1 200 OK";
+fn find_host(request: &Vec<String>) -> Option<String> {//TODO - Proper request parsing.
+    request.iter()
+        .find(|line| line.to_lowercase().starts_with("host: "))//TODO - Change case in parsing.
+        .map(|s| s[6..].to_string())
+}
 
-    return format!("{status_line}\r\n{headers}\r\n\r\n{content}");
+fn response_host_not_specified() -> String {
+    format!(
+        "{}\r\n{}\r\n\r\n{}",
+        "HTTP/1.1 400 Bad Request",
+        format!("Content-Type: text/plain"),
+        "Host not specified in request headers",
+    )
+}
+
+fn response_host_not_configured(host: &str) -> String {
+    format!(
+        "{}\r\n{}\r\n\r\n{}",
+        "HTTP/1.1 404 Not Found",
+        format!("Content-Type: text/plain"),
+        format!("Host not configured: {}", host),
+    )
+}
+
+fn response_redirect(location: &str) -> String {
+    format!(
+        "{}\r\n{}\r\n\r\n",
+        "HTTP/1.1 308 Permanent Redirect",
+        format!("Location: {location}"),
+    )
 }
 
